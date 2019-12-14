6189b8d17ea392a0e31cc0c135196b8558426af2
Francis Duvivier <francis.duvivier@theoplayer.com>
2019-12-14 19:53:21 +0100

    Add R solution 4 part 2

--- day4/solution4.R
+++ day4/solution4.R
@@ -5,10 +5,10 @@ number <- 0
 repeat {
   a <- paste(input, number, sep = "")
   res <- digest::digest(a, algo = "md5", serialize = FALSE)
-  r <- strsplit(res, "")
-  if (r[[1]][1]=="0" && r[[1]][2]=="0" && r[[1]][3]=="0" && r[[1]][4]=="0" && r[[1]][5]=="0")  {
+  if (substr(res, 1, 6) == "000000") {
     break
   }
+
   number <- number + 1
 }
 
