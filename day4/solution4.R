3936b4fe79cf99b65aec6c3ec2b1b36513782389
Francis Duvivier <francis.duvivier@theoplayer.com>
2019-12-14 19:14:02 +0100

    Add R solution 4

---
+++ day4/solution4.R
@@ -0,0 +1,15 @@
+input <- "bgvyzdsv"
+
+
+number <- 0
+repeat {
+  a <- paste(input, number, sep = "")
+  res <- digest::digest(a, algo = "md5", serialize = FALSE)
+  r <- strsplit(res, "")
+  if (r[[1]][1]=="0" && r[[1]][2]=="0" && r[[1]][3]=="0" && r[[1]][4]=="0" && r[[1]][5]=="0")  {
+    break
+  }
+  number <- number + 1
+}
+
+print(number)
